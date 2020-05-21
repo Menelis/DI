1c83e4009515b8cd58c5ef3d1a8efa671eae507c
Menelis mthembu <menelismthembu@gmail.com>
2020-05-22 00:46:47 +0200

    Update Filter propery to readonly

--- DI.Common/Specifications/BaseSpecification.cs
+++ DI.Common/Specifications/BaseSpecification.cs
@@ -12,6 +12,6 @@ namespace DI.Common.Specifications
         {
             Filter = filter;
         }
-        public Expression<Func<T, bool>> Filter { get; set; }
+        public Expression<Func<T, bool>> Filter { get; }
     }
 }
